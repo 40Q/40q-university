f2e02b288de0b31204b886604fbbd1f7955f18ed
Tomás Siegfried <tsiegfried@40q.agency>
2024-03-04 12:45:38 -0300

    CU-86azgqj2b fix banner width desktop

--- resources/views/blocks/banner.blade.php
+++ resources/views/blocks/banner.blade.php
@@ -1,7 +1,7 @@
 <section>
     <div class="relative py-20 lg:pt-12 lg:pb-11 px-1 md:px-17">
         <div class="relative z-50 text-white flex flex-col items-center justify-center lg:justify-start lg:items-start gap-3 lg:gap-4">
-            <x-section-header containerClasses="sm:w-3/4 text-center lg:text-start lg:w-auto" headingClasses="lg:w-1/2 tracking-wider" textClasses="lg:w-1/2 lg:pr-20" :showEyebrow="$showEyebrow" :eyebrow="$eyebrow" :showHeading="$showHeading" :heading="$heading" :showText="$showText"
+            <x-section-header containerClasses="sm:w-3/4 text-center lg:text-start lg:w-full" headingClasses="lg:w-1/2 tracking-wider" textClasses="lg:w-1/2 lg:pr-20" :showEyebrow="$showEyebrow" :eyebrow="$eyebrow" :showHeading="$showHeading" :heading="$heading" :showText="$showText"
                 :text="$text" />
             @if ($buttonText)
                 <x-button :text="$buttonText" :href="$buttonHref" />
